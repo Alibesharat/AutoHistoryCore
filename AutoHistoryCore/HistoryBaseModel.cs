09e400de5f6031cc5e345c65b3540b3016077fc9
Ali Besharati <1368.ali.b@gmail.com>
2018-12-22 11:03:36 +0330

    Add DisplayName

--- AutoHistoryCore/HistoryBaseModel.cs
+++ AutoHistoryCore/HistoryBaseModel.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace AutoHistoryCore
 {
-   public class HistoryBaseModel
+    public class HistoryBaseModel
     {
+        [Display(Name ="تاریخ ثبت")]
         public DateTime? CrearedDateTime { get; set; }
+        [Display(Name = "تاریخ حذف")]
         public DateTime? DeletedDatTime { get; set; }
+        [Display(Name = "تاریخ آخرین ویرایش")]
         public DateTime? LastEditedDateTime { get; set; }
+        [Display(Name = "حذف شده ؟")]
         public bool IsDeleted { get; set; }
     }
 }
