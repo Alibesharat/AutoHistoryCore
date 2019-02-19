f259886afa8ba9ec6b055700479dba42d4330d91
Ali Besharati <1368.ali.b@gmail.com>
2019-02-19 15:39:13 +0330

    Add Async Methode

--- AutoHistoryCore/HistoryContext.cs
+++ AutoHistoryCore/HistoryContext.cs
@@ -1,9 +1,7 @@
-﻿using JetBrains.Annotations;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutoHistoryCore
 {
@@ -16,11 +14,14 @@ namespace AutoHistoryCore
         /// <param name="set"></param>
         /// <returns></returns>
         public static IQueryable<TEntity> Undelited<TEntity>(this DbSet<TEntity> set)
-       where TEntity :  HistoryBaseModel
+       where TEntity : HistoryBaseModel
         {
+          
             var data = set.AsNoTracking().Where(e => e.IsDeleted == false);
             return data.AsQueryable();
         }
+
+   
 
         /// <summary>
         /// ثبت تفییرات کانتکس با تاریخچه
@@ -37,7 +38,7 @@ namespace AutoHistoryCore
                 {
                     HistoryBaseModel model = (HistoryBaseModel)entity.Entity;
 
-                   
+
                     switch (entity.State)
                     {
 
@@ -72,6 +73,58 @@ namespace AutoHistoryCore
             }
 
             return db.SaveChanges();
+        }
+
+
+        /// <summary>
+        /// ثبت تفییرات کانتکس با تاریخچه
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        public async static Task<int> SaveChangesWithHistoryAsync(this DbContext db)
+        {
+            var entries = db.ChangeTracker.Entries().ToArray();
+
+            foreach (var entity in entries)
+            {
+                try
+                {
+                    HistoryBaseModel model = (HistoryBaseModel)entity.Entity;
+
+
+                    switch (entity.State)
+                    {
+
+                        case EntityState.Detached:
+                            break;
+                        case EntityState.Unchanged:
+                            break;
+                        case EntityState.Deleted:
+                            model.IsDeleted = true;
+                            model.DeletedDatTime = DateTime.Now;
+                            entity.State = EntityState.Modified;
+                            break;
+                        case EntityState.Modified:
+                            model.IsDeleted = false;
+                            model.LastEditedDateTime = DateTime.Now;
+                            break;
+                        case EntityState.Added:
+                            model.IsDeleted = false;
+                            model.CrearedDateTime = DateTime.Now;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch
+                {
+
+                    ;
+                }
+
+            }
+
+            return await db.SaveChangesAsync();
         }
     }
 }
