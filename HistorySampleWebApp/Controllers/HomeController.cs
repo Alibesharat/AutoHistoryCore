72d03c80ab6306ff0930c253dbd9f93a64547b0b
Ali Besharati <1368.ali.b@gmail.com>
2020-03-27 16:03:42 +0430

    remove sample app

--- HistorySampleWebApp/Controllers/HomeController.cs
+++ HistorySampleWebApp/Controllers/HomeController.cs
@@ -87,24 +87,7 @@ namespace HistorySampleWebApp.Controllers
 
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
